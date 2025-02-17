05393188810e53036d28486c0cfabf927781c708
Hayk7JS7 <118001589+Hayk7JS7@users.noreply.github.com>
2025-02-17 21:16:43 +0400

    Update main.cpp
    Do some loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 int main() {
-    std::cout << "Hello from CMake!" << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "The index is: " << i << std::endl;
+    }
     return 0;
 }
